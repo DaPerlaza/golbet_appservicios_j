b585bbb912ab5a87507e11396143256565841dee
DANIELA PERLAZA ORJUELA <danielaperlaza1138532@correo.itm.edu.co>
2026-09-15 14:39:25 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

---
+++ GolBet.Services/Mapping/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿// GolBet.Services/Mapping/MappingProfile.cs 
+
+using AutoMapper;
+
+using GolBet.Entities;
+
+using GolBet.Services.DTOs;
+
+namespace GolBet.Services.Mapping;
+public class MappingProfile : Profile
+
+{
+
+    public MappingProfile()
+
+    {
+
+        CreateMap<Match, MatchDto>();
+
+    }
+
+}
